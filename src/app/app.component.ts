e60dbcaed2ed93dea833099e1d7cafe86bd0ee4e
jgarea <jgntra@gmail.com>
2024-07-03 15:48:14 +0200

    update todo.component *ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from './pages/todo/todo.component';
+import { TODO_DATA } from '../assets/todo';
+import { NTodo } from './models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -13,5 +15,14 @@ import { TodoComponent } from './pages/todo/todo.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-18-app';
+  todoData1= TODO_DATA[0];
+  todoData2= TODO_DATA[1];
+  todoData3= TODO_DATA[2];
+  todoData4= TODO_DATA[3];
+  todoData5= TODO_DATA[4];
+  todoData6= TODO_DATA[10];
+
+  getTodoInfo(val: NTodo.TodoData){
+    console.log('Click en icono: ', val);
+  }
 }
