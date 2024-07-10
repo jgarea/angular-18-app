98f5ff7051b7473657ca99c7dad511f426412a90
jgarea <jgntra@gmail.com>
2024-07-10 19:30:14 +0200

    sesion 4 initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from './pages/todo/todo.component';
 import { TODO_DATA } from '../assets/todo';
 import { NTodo } from './models/todo.model';
 import { CommonModule } from '@angular/common';
-import { HighlighedDirective } from './directives/highlighed.directive';
-import { NgxUnlessDirective } from './directives/ngx-unless.directive';
 
 @Component({
   selector: 'app-root',
@@ -13,37 +11,17 @@ import { NgxUnlessDirective } from './directives/ngx-unless.directive';
   imports: [
     RouterOutlet,
     TodoComponent,
-    CommonModule,
-    HighlighedDirective,
-    NgxUnlessDirective
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements AfterViewInit{
-  title = '';
-  todoData = TODO_DATA[0];
+export class AppComponent {
+  todos = TODO_DATA;
 
   constructor() {}
-  ngAfterViewInit(): void {
-    // console.log(this.highlighedDirective);
-  }
-
-  // @ViewChild(HighlighedDirective) highlighedDirective!: HighlighedDirective;
-  @ViewChild('todoRef',{read: HighlighedDirective}) highlighedDirective!: HighlighedDirective;
 
   getTodoInfo(val: NTodo.TodoData) {
     console.log(val);
-  }
-
-  trackByFn(_index: number, item: NTodo.TodoData) {
-    return item.id;
-  }
-
-  // orderData() {
-  //   this.todoData.sort((a, b) => a.priority -  b.priority);
-  // }
-  onToggleHighlighted(highlighted: boolean) {
-    console.log(highlighted);
   }
 }
--- src/app/directives/highlighed.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlighedDirective } from './highlighed.directive';
-
-describe('HighlighedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlighedDirective();
-    expect(directive).toBeTruthy();
-  });
-});
