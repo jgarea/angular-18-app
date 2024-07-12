bc5d33f67ed92a53d8aa526f45895775fbc8e3a2
jgarea <jgntra@gmail.com>
2024-07-12 15:07:53 +0200

    httpclient delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,15 +32,41 @@ export class AppComponent implements OnInit{
   constructor(
 private readonly apiService: ApiService  ) {}
   ngOnInit(): void {
+    this.getTodos();
+  }
+
+  private getTodos(){
     this.apiService.get<NTodo.TodosResponse>().subscribe(val => this.todos = val);
   }
  
 
-  getTodoInfo(val: NTodo.TodoData) {
-    console.log(val);
+  deleteTodo(item: NTodo.TodoData) {
+    this.apiService.delete<NTodo.TodosResponse>(item.id).subscribe(todos=> this.todos = todos);
   }
 
   updateTodo(item: NTodo.TodoData){
-    this.apiService.put(item,item.id).subscribe(console.log);
+    // this.apiService.put(item,item.id).subscribe(console.log);
+    this.apiService.patch({description: item.description},item.id).subscribe(console.log);
+  }
+  addTodo(){
+    this.apiService.post({
+      "title": "Leer documentación técnica",
+      "description": "Investigar y leer la documentación de una nueva tecnología o herramienta relevante para el proyecto.",
+      "status": "Por hacer",
+      "priority": 3,
+      "hidden": false,
+      "id": 1,
+      "deadLine": "2024-04-07T03:25:54.898Z",
+      "color": {
+        "status": "#ed4040",
+        "priority": "#68db68"
+      },
+      "class": {
+        "status": "to-do",
+        "priority": "low"
+      },
+      "progress": 0.2
+    }).subscribe(()=> this.getTodos());
+
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { NTodo } from '../models/todo.model';
   providedIn: 'root'
 })
 export class ApiService {
-  private readonly baseUrl='http://localhost:3000/todos';
+  private readonly baseUrl = 'http://localhost:3000/todos';
 
 
   constructor(
@@ -14,14 +14,23 @@ export class ApiService {
   ) { }
   get<T>() {
     const headers = new HttpHeaders()
-    .set('my-token','tokenValue')
+      .set('my-token', 'tokenValue')
     // this.http.get(this.baseUrl).subscribe(console.log);
-    return this.http.get<T>(this.baseUrl,{ headers});
+    return this.http.get<T>(this.baseUrl, { headers });
   }
 
-put<P, R>(payload: P,id: number) {
-  const params = new HttpParams()
-  .set('date','2021-09-01');
-   return this.http.put<R>(this.baseUrl + '/' + id,payload ,{ params});
-}
+  put<P, R>(payload: P, id: number) {
+    const params = new HttpParams()
+      .set('date', '2021-09-01');
+    return this.http.put<R>(this.baseUrl + '/' + id, payload, { params });
+  }
+  patch<P, R>(payload: P, id: number) {
+    return this.http.patch<R>(this.baseUrl + '/' + id, payload);
+  }
+  post<P, R>(payload: P) {
+    return this.http.post<R>(this.baseUrl , payload);
+  }
+  delete<T>(id: number) {
+    return this.http.delete<T>(this.baseUrl + '/' + id);
+  }
 }
